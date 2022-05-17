f00d6153955836c53696ed7b5ec92063c2b7695b
sorude26 <sorude26@gmail.com>
2022-05-17 17:37:23 +0900

    ステージのメッシュを結合させる処理を追加した

--- Assets/MyGame/Scripts/Stage/StageCreator.cs
+++ Assets/MyGame/Scripts/Stage/StageCreator.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class StageCreator : MonoBehaviour
 {
     [SerializeField]
+    private Transform _base = default;
+    [SerializeField]
     private GameObject _stagePrefab = default;
     [SerializeField]
     private float _stageScale = 1f;
     [SerializeField]
     private int _maxSize = 15;
+    [SerializeField]
+    private float _level = 0.5f;
     public void CreateStage()
     {
         for (int y = 0; y < _maxSize; y++)
         {
             for (int x = 0; x < _maxSize; x++)
             {
-                var stage = Instantiate(_stagePrefab, transform);
-                stage.transform.position = new Vector3(_stageScale * x, 0, _stageScale * y);
+                var stage = Instantiate(_stagePrefab, _base);
+                stage.transform.position = new Vector3(_stageScale * x, Random.Range(0,_level), _stageScale * y);
             }
         }
-
+        MeshControl.Combine(transform);
+        _base.gameObject.SetActive(false);
     }
 }
