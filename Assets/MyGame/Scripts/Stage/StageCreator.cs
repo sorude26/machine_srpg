0fbbe171e82eb8d3f3600be4809ec29c288105f1
sorude26 <sorude26@gmail.com>
2022-05-19 13:48:37 +0900

    実際に使用するステージのエリア設定とPrefab調整を行った

--- Assets/MyGame/Scripts/Stage/StageCreator.cs
+++ Assets/MyGame/Scripts/Stage/StageCreator.cs
@@ -7,7 +7,11 @@ public class StageCreator : MonoBehaviour
     [SerializeField]
     private Transform _base = default;
     [SerializeField]
-    private StagePoint _stagePrefab = default;
+    private Transform _stageBase = default;
+    [SerializeField]
+    private StagePoint _pointPrefab = default;
+    [SerializeField]
+    private GameObject _stagePrefab = default;
     [SerializeField]
     private float _stageScale = 1f;
     [SerializeField]
@@ -16,18 +20,41 @@ public class StageCreator : MonoBehaviour
     private int _level = 2;
     [SerializeField]
     private float _scale = 0.5f;
+    private int[] _levels = default;
+    private int _stageSizeX = default;
+    private int _stageSizeY = default;
     public void CreateStage()
     {
-        for (int y = 0; y < _maxSize; y++)
+        int startX = (_stageSizeX - _maxSize) / 2;
+        int startY = (_stageSizeY - _maxSize) / 2;
+        for (int y = startY; y < startY + _maxSize; y++)
         {
-            for (int x = 0; x < _maxSize; x++)
+            for (int x = startX; x < startX + _maxSize; x++)
             {
-                var stage = Instantiate(_stagePrefab, _base);
-                float level = Random.Range(0, _level) * _scale;
+                var stage = Instantiate(_pointPrefab, _stageBase);
+                float level = _levels[x + y * _stageSizeX] * _scale; ;
                 stage.transform.position = new Vector3(_stageScale * x, level, _stageScale * y);
-                stage.Pos = (x, y, level);
+                stage.Pos = (x - startX, y - startY, level);
             }
         }
+        //MeshControl.Combine(transform);
+    }
+    public void CreateStage(int sizeX,int sizeY, int[] levels)
+    {
+        _levels = levels;
+        for (int y = 0; y < sizeY; y++)
+        {
+            for (int x = 0; x < sizeX; x++)
+            {
+                var stage = Instantiate(_stagePrefab, _base);
+                float level = _levels[x + y * sizeX] * _scale;
+                stage.transform.position = new Vector3(_stageScale * x, level, _stageScale * y);
+            }
+        }
+        _stageSizeX = sizeX;
+        _stageSizeY = sizeY;
         MeshControl.Combine(transform);
+        _base.gameObject.SetActive(false);
+        CreateStage();
     }
 }
--- Assets/MyGame/Scripts/Stage/StagePoint.cs
+++ Assets/MyGame/Scripts/Stage/StagePoint.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 
 public class StagePoint : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _selectMark = default; 
     public (int x, int y,float level) Pos;
     public event Action<(int, int,float)> DelSelect = default;
     private void OnMouseDown()
     {
         Debug.Log(Pos);
+        OpenMark();
         DelSelect?.Invoke(Pos);
+    }
+    public void OpenMark()
+    {
+        if (_selectMark != null)
+        {
+            _selectMark.SetActive(true);
+        }
+    }
+    public void CloseMark()
+    {
+        if (_selectMark != null)
+        {
+            _selectMark.SetActive(false);
+        }
     }
 }
