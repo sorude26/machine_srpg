8e2c2f0911bf68d3d1b6bbacf204711dcefed4ce
sorude26 <sorude26@gmail.com>
2022-05-20 19:16:13 +0900

    追従機能スクリプトを追加した

---
+++ Assets/MyGame/Scripts/FollowObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    [SerializeField]
+    Transform _followTarget = default;
+    [SerializeField]
+    Transform _rotationTarget = default;
+    [SerializeField]
+    float _followSpeed = 1f;
+    [SerializeField]
+    float _rotationSpeed = 1f;
+    private void FixedUpdate()
+    {
+        if (_followTarget == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        transform.forward = Vector3.Lerp(transform.forward, _rotationTarget.forward, _rotationSpeed * Time.deltaTime);
+        float speed = (transform.position - _followTarget.position).sqrMagnitude;
+        transform.position = Vector3.Lerp(transform.position, _followTarget.position, speed * _followSpeed * Time.deltaTime);
+    }
+}
