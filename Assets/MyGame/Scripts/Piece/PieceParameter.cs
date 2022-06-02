a47147b236830b0a6fb5eb873e03ae00ffdc76ed
sorude26 <sorude26@gmail.com>
2022-06-02 19:33:34 +0900

    駒のパラメータクラスを追加した

---
+++ Assets/MyGame/Scripts/Piece/PieceParameter.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceParameter : MonoBehaviour
+{
+    public float Activity { get; private set; }
+}
