951a8d5561cd67c196d4748df8a1f264ec92748d
sorude26 <sorude26@gmail.com>
2022-08-15 22:14:05 +0900

    武器データクラスを追加した

---
+++ Assets/MyGame/Scripts/Piece/WeaponData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponData
+{
+    public int Range;
+    public int LevelRange;
+    public int Power;
+    public int Type;
+    public int Hit;
+    public int Count;
+}
